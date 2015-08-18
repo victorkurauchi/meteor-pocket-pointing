970bd8de7e1223446111d315d6fd053908130ef8
Victor Kurauchi <victorkurauchi@gmail.com>
2015-08-17 23:55:54 -0300

    simple rest

---
+++ server/api.js
@@ -0,0 +1,27 @@
+// Enable cross origin requests for all endpoints
+JsonRoutes.setResponseHeaders({
+  "Cache-Control": "no-store",
+  "Pragma": "no-cache",
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "GET, PUT, POST, DELETE, OPTIONS",
+  "Access-Control-Allow-Headers": "Content-Type, Authorization, X-Requested-With"
+});
+
+Meteor.publish("api/employees", function () {
+  return Employees.find();
+});
+
+Meteor.method("add-numbers", function (a, b) {
+  return a + b;
+}, {
+  url: "add-numbers",
+  getArgsFromRequest: function (request) {
+    // Let's say we want this function to accept a form-encoded request with
+    // fields named `a` and `b`.
+    var content = request.body;
+
+    // Since form enconding doesn't distinguish numbers and strings, we need
+    // to parse it manually
+    return [ parseInt(content.a, 10), parseInt(content.b, 10) ];
+  }
+})
